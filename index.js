96251298d4bd1680154a16ca2cd96695f3c654b7
cavis <ryancavis@gmail.com>
2021-05-18 08:46:43 -0600

    Setup and test

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-function handler(event) { return response; }
+function handler(event) {
+  var DOVETAIL_HOST = 'https://dovetail.prxu.org/_';
+  var request = event.request || {};
+  var querystring = request.querystring || {};
+  var uri = request.uri || '';
+
+  // pass-through root/favicon
+  if (['', '/', '/index.html', '/favicon.ico'].includes(uri)) {
+    return request;
+  }
+
+  // just kick out invalid looking paths
+  // either: /podcast_id/episode_guid/digest/filename.mp3
+  //     or: /podcast_id/feed_id/episode_guid/digest/filename.mp3
+  var parts = uri.split('/').filter(p => p);
+  if (parts.length !== 4 && parts.length !== 5) {
+    return { statusCode: 404, statusDescription: 'Not found. Like, ever.' };
+  }
+
+  // kick back expired redirects (TODO: require ?exp later on)
+  if (querystring.exp) {
+    var now = Math.round(Date.now() / 1000);
+    if (now > parseInt(querystring.exp.value, 10)) {
+      parts.splice(-2, 1); // digest is always 2nd to last
+      var headers = { location: { value: `${DOVETAIL_HOST}/${parts.join('/')}` } };
+      return { headers, statusCode: 302, statusDescription: 'Arrangement expired' };
+    }
+  }
+
+  // TODO: check/require a signature query param (signing your path/exp/le)
+  // TODO: some sort of ?force requests to re-stitch (maybe add a uuid to uri?)
+
+  // normalize stitch requests to /<podcast_id>/<episode_guid>/<digest>
+  if (parts.length === 5) {
+    parts.splice(1, 1);
+  }
+  parts.splice(-1, 1);
+  request.uri = '/' + parts.join('/');
+
+  return request;
+}
---
+++ index.test.js
@@ -0,0 +1,75 @@
+eval(require('fs').readFileSync('./index.js').toString());
+
+describe('handler', () => {
+  function event(uri = '', querystring = {}) {
+    return { request: { uri, querystring, method: 'GET', headers: {} } };
+  }
+
+  // call the handler, duplicating the event so it's not mutated
+  function callHandler(event) {
+    return handler(JSON.parse(JSON.stringify(event)));
+  }
+
+  let now;
+  beforeEach(() => (now = Math.round(Date.now() / 1000)));
+
+  it('passes through root paths', async () => {
+    expect(handler(event())).toEqual(event().request);
+    expect(handler(event(''))).toEqual(event('').request);
+    expect(handler(event('/'))).toEqual(event('/').request);
+    expect(handler(event('/index.html'))).toEqual(event('/index.html').request);
+    expect(handler(event('/favicon.ico'))).toEqual(event('/favicon.ico').request);
+  });
+
+  it('404s on bad looking paths', async () => {
+    expect(handler(event('/some')).statusCode).toEqual(404);
+    expect(handler(event('/some/path')).statusCode).toEqual(404);
+    expect(handler(event('/some/path/here')).statusCode).toEqual(404);
+  });
+
+  it('allows non-expiring links through', async () => {
+    const event1 = event('/1234/some-guid/some-digest/file.mp3');
+    expect(handler(event1)).toEqual(event1.request);
+
+    const event2 = event('/1234/adfree/some-guid/some-digest/file.mp3');
+    expect(handler(event2)).toEqual(event2.request);
+  });
+
+  it('allows non-expired links through', async () => {
+    const exp = { value: now + 10 };
+
+    const event1 = event('/1234/some-guid/some-digest/file.mp3', { exp });
+    expect(handler(event1)).toEqual(event1.request);
+
+    const event2 = event('/1234/adfree/some-guid/some-digest/file.mp3', { exp });
+    expect(handler(event2)).toEqual(event2.request);
+  });
+
+  it('redirects expired links', async () => {
+    const exp = { value: now - 10 };
+
+    const event1 = event('/1234/some-guid/some-digest/file.mp3', { exp });
+    expect(handler(event1).statusCode).toEqual(302);
+    expect(handler(event1).headers.location.value).toEqual(
+      'https://dovetail.prxu.org/_/1234/some-guid/file.mp3',
+    );
+
+    const event2 = event('/1234/adfree/some-guid/some-digest/file.mp3', { exp });
+    expect(handler(event2).statusCode).toEqual(302);
+    expect(handler(event2).headers.location.value).toEqual(
+      'https://dovetail.prxu.org/_/1234/adfree/some-guid/file.mp3',
+    );
+  });
+
+  it('removes filenames and feed-ids from uris', async () => {
+    const exp = { value: now + 10 };
+
+    const event1 = event('/1234/some-guid/some-digest/file.mp3', { exp });
+    const result1 = handler(JSON.parse(JSON.stringify(event1)));
+    expect(result1.uri).toEqual('/1234/some-guid/some-digest');
+
+    const event2 = event('/1234/adfree/some-guid/some-digest/file.mp3', { exp });
+    const result2 = handler(JSON.parse(JSON.stringify(event2)));
+    expect(result2.uri).toEqual('/1234/some-guid/some-digest');
+  });
+});
