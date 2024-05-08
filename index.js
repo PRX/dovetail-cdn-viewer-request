7d9574290f709be53dadca9a510e9d358810ae9c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2024-05-08 18:16:06 -0400

    Separate digest and transcode handling

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function handler(event) {
-  var EXPIRED_REDIRECT_PREFIX = '<EXPIRED_REDIRECT_PREFIX>';
   var request = event.request || {};
-  var querystring = request.querystring || {};
   var uri = request.uri || '';
 
   // pass-through root/favicon
@@ -16,21 +14,25 @@ function handler(event) {
     parts.shift();
   }
 
-  // just kick out invalid looking paths
+  if (parts[0] === 't') {
+    return handleTranscode(request, parts);
+  } else {
+    return handleDigest(request, parts);
+  }
+}
 
+function handleDigest(request, parts) {
+  var EXPIRED_REDIRECT_PREFIX = '<EXPIRED_REDIRECT_PREFIX>';
+
+  // just kick out invalid looking paths
   // /podcast_id/episode_guid/digest/filename.mp3
   // /podcast_id/feed_id/episode_guid/digest/filename.mp3
-  // /t/a/path/<file>/<format>.<extension>
-  // /t/<host>/[<path>/]<file>/<format>.<extension>
-  if (parts[0] === 't') {
-    if (parts.length < 4) {
-      return { statusCode: 404, statusDescription: 'Not found. Nope.' };
-    }
-  } else if (parts.length !== 4 && parts.length !== 5) {
+  if (parts.length !== 4 && parts.length !== 5) {
     return { statusCode: 404, statusDescription: 'Not found. Like, ever.' };
   }
 
   // kick back expired redirects (TODO: require ?exp later on)
+  var querystring = request.querystring || {};
   if (querystring.exp) {
     var now = Math.round(Date.now() / 1000);
     if (now > parseInt(querystring.exp.value, 10)) {
@@ -49,24 +51,34 @@ function handler(event) {
 
   // TODO: check/require a signature query param (signing your path/exp/le/force)
   // TEMPORARY: just kick out short/fake looking digests (2nd to last)
-  if (parts[0] !== 't') {
-    var digest = parts[parts.length - 2];
-    if (digest.length < 20 && digest !== 'some-digest') {
-      return { statusCode: 404, statusDescription: 'Not found. Like, what?' };
-    }
+  var digest = parts[parts.length - 2];
+  if (digest.length < 20 && digest !== 'some-digest') {
+    return { statusCode: 404, statusDescription: 'Not found. Like, what?' };
+  }
 
-    // normalize stitch requests to /<podcast_id>/<episode_guid>/<digest>
-    if (parts.length === 5) {
-      parts.splice(1, 1);
-    }
-    parts.splice(-1, 1);
-    request.uri = '/' + parts.join('/');
+  // normalize stitch requests to /<podcast_id>/<episode_guid>/<digest>
+  if (parts.length === 5) {
+    parts.splice(1, 1);
+  }
+  parts.splice(-1, 1);
+  request.uri = '/' + parts.join('/');
 
-    // force restitching the arrangement by adding a random string to the url
-    // TODO: REAAAALLY need to sign/secret this one somehow
-    if (querystring.force) {
-      request.uri += `/force-${Date.now()}`;
-    }
+  // force restitching the arrangement by adding a random string to the url
+  // TODO: REAAAALLY need to sign/secret this one somehow
+  var querystring = request.querystring || {};
+  if (querystring.force) {
+    request.uri += `/force-${Date.now()}`;
+  }
+
+  return request;
+}
+
+function handleTranscode(request, parts) {
+  // just kick out invalid looking paths
+  // /t/a/path/<file>/<format>.<extension>
+  // /t/<host>/[<path>/]<file>/<format>.<extension>
+  if (parts.length < 4) {
+    return { statusCode: 404, statusDescription: 'Not found. Nope.' };
   }
 
   return request;
